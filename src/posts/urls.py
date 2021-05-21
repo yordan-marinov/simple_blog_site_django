da88f847f83d2f11d33b5aa83bf5b4c5f9521386
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-21 20:19:56 +0300

    Added all the paths from the views file

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import post_list
+from .views import post_list, post_detail, post_create, post_update, post_delete
 
 urlpatterns = [
     path("", post_list, name="post_list"),
+    path("create/", post_create, name="post_create"),
+    path("<int:pk>/", post_detail, name="post_detail"),
+    path("<int:pk>/update/", post_update, name="post_update"),
+    path("<int:pk>/delete/", post_delete, name="post_delete"),
 ]
